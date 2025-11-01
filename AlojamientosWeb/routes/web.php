04cbd154bbda29e4cde5dd1104a5d04c62f6dd6b
kevin19sanchez <kasc1996.sanchez@gmail.com>
2025-10-31 23:56:23 -0600

    archivo route modificada

--- AlojamientosWeb/routes/web.php
+++ AlojamientosWeb/routes/web.php
@@ -13,8 +13,8 @@ Route::get('/', function () {
 
 //RUTAS LOGIN & REGISTER
 Route::get('/login', [UsuarioController::class, 'index'])->name('login');
-Route::get('/register', [UsuarioController::class, 'indexR'])->name('register');
 Route::post('/login', [UsuarioController::class, 'login'])->name('login');
+Route::get('/register', [UsuarioController::class, 'indexR'])->name('register');
 Route::post('/logout', [UsuarioController::class, 'logout'])->name('logout');
 //RUTAS LOGIN & REGISTER END
 
@@ -26,28 +26,32 @@ Route::post('/createuser', [UsuarioController::class, 'store'])->name('create.us
 Route::get('/home', [AlojamientoController::class, 'index'])->name('home');
 //RUTA PARA LA PAGINA LANDING PAGE END
 
+//RESERVA
+Route::get('/detail/{id}', [ReservaController::class, 'show'])->name('detail.show');
+
 Route::get('/lodging', [TiposAlojamientoController::class, 'index'])->name('index.lodging');
-
-//RUTAS CREAR TIPOS ALOJAMIENTOS
-Route::post('/createlodging', [TiposAlojamientoController::class, 'store'])->name('create.typelodging');
-Route::put('/editlodging/{id}', [TiposAlojamientoController::class, 'update'])->name('edit.typelodging');
-Route::delete('/deletelodging/{id}', [TiposAlojamientoController::class, 'destroy'])->name('delete.typelodging');
-
-//RUTAS CREAR ALOJAMINETOS
-Route::post('/createalojamiento', [AlojamientoController::class, 'store'])->name('create.alojamineto');
-Route::put('/editalojamiento/{id}', [AlojamientoController::class, 'update'])->name('edit.alojamiento');
-//Route::delete('/deletealojamiento/{id}', [AlojamientoController::class, 'destroy'])->name('delete.alojamiento');
 
 //RESEÑA
 Route::get('/alojamiento/{alojamiento}/review', [ReseñaController::class, 'index'])->name('review.index');
-Route::post('/createreview',  [ReseñaController::class, 'store'])->name('review.create');
 
-//RESERVA
-Route::get('/detail/{id}', [ReservaController::class, 'show'])->name('detail.show');
 
 Route::middleware('auth')->group(function() {
     Route::get('/mi-cuenta', [ReservaController::class, 'misReservas'])->name('reserva.index');
     Route::post('/reservas', [ReservaController::class, 'store'])->name('reserva.create');
     Route::delete('/reserva/{reserva}', [ReservaController::class, 'destroy'])->name('reserva.delete');
+    Route::post('/createreview',  [ReseñaController::class, 'store'])->name('review.create');
 });
+
+Route::middleware(['auth', 'admin'])->group(function () {
+    //RUTAS CREAR TIPOS ALOJAMIENTOS
+    Route::post('/createlodging', [TiposAlojamientoController::class, 'store'])->name('create.typelodging');
+    Route::put('/editlodging/{id}', [TiposAlojamientoController::class, 'update'])->name('edit.typelodging');
+    Route::delete('/deletelodging/{id}', [TiposAlojamientoController::class, 'destroy'])->name('delete.typelodging');
+
+    //RUTAS CREAR ALOJAMINETOS
+    Route::post('/createalojamiento', [AlojamientoController::class, 'store'])->name('create.alojamineto');
+    Route::put('/editalojamiento/{id}', [AlojamientoController::class, 'update'])->name('edit.alojamiento');
+    //Route::delete('/deletealojamiento/{id}', [AlojamientoController::class, 'destroy'])->name('delete.alojamiento');
+});
+
 
